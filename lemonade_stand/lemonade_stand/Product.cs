daa5449d52e20a37a79b28e1f34bc836242bd80b
manzilat <amtulnoorq@yahoo.com>
2018-09-27 09:35:40 -0500

    created new class for player, daily report and day where we give initial amount to the player and get the details for all day

---
+++ lemonade_stand/lemonade_stand/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lemonade_stand
+{
+    class Product
+    {
+        string name;
+        string unit;
+        string recipepart;
+        double price;
+       
+        
+        public Product(string name, double price, string unit, string recipepart)
+        {
+            this.name = name;
+            this.unit = unit;
+            this.recipepart = recipepart;
+            this.price = price;
+           
+        }
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            private set { price = value; }
+        }
+
+        public string Recipepart
+        {
+            get { return recipepart; }
+            private set { recipepart = value; }
+        }
+
+        public string Unit
+        {
+            get { return unit; }
+            private set { unit = value; }
+        }
+    }
+}
